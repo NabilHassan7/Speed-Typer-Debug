e9f4c9089d99b2226b56a62f77b5ca33e5e9e922
NabilHassan7 <fakrulibnehassan@gmail.com>
2022-09-15 16:21:40 +0600

    changed the time displayed to always be an integer

--- script.js
+++ script.js
@@ -68,7 +68,8 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  // Added Math.floor to round out the time taken
+  const timeTaken = Math.floor((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -138,7 +139,8 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  // Added Math.floor() to round out the time taken
+  const timeSpent = Math.floor((currentTime - startTime) / 1000);
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
